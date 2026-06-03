1e8780a54f2cf98746fdb4ecde4c692ecd8e9bf4
fish <1063145025@qq.com>
2026-06-03 23:55:17 +0800

    屏蔽暂时不用的 adc/dac 指令以节省 Flash/RAM 空间

--- Source/SCMD/App/scmd_config.c
+++ Source/SCMD/App/scmd_config.c
@@ -8,8 +8,8 @@
 #include "scmd_io.h"
 #include "scmd_i2c.h"
 #include "scmd_spi.h"
-#include "scmd_adc.h"
-#include "scmd_dac.h"
+//#include "scmd_adc.h"
+//#include "scmd_dac.h"
 #include "scmd_pwm.h"
 #include "scmd_capture.h"
 
@@ -33,8 +33,8 @@ scmd_cmd_def scmd_list[] =
 		{.func = scmd_io,		.name = "io",		.dest = ">io help",			.isVisible = 1,},
 		{.func = scmd_i2c,		.name = "i2c",		.dest = ">i2c help",		.isVisible = 1,},
 		{.func = scmd_spi,		.name = "spi",		.dest = ">spi help",		.isVisible = 1,},
-		{.func = scmd_adc,		.name = "adc",		.dest = ">adc help",		.isVisible = 1,},
-		{.func = scmd_dac,		.name = "dac",		.dest = ">dac help",		.isVisible = 1,},
+/*		{.func = scmd_adc,		.name = "adc",		.dest = ">adc help",		.isVisible = 1,},*/
+/*		{.func = scmd_dac,		.name = "dac",		.dest = ">dac help",		.isVisible = 1,},*/
 		{.func = scmd_pwm,		.name = "pwm",		.dest = ">pwm help",		.isVisible = 1,},
 		{.func = scmd_capture,	.name = "capture",	.dest = ">capture help",	.isVisible = 1,},
 		{.func = scmd_dvm,		.name = "dvm",		.dest = ">dvm help",		.isVisible = 1,},
